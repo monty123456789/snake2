7b0d7207ac755ab8294843368dcab97f1ba4123c
monty123456789 <monty.chaudoir@gmail.com>
2021-01-20 12:41:12 +0000

    player moving fluid but only one at a time

--- twoplayersnake3.py
+++ twoplayersnake3.py
@@ -41,7 +41,7 @@ class Snake:
         self.head.shape('arrow')
         self.head.penup()
         self.head.pensize(2)
-        self.head.speed(0)#
+        self.head.speed(1)
         self.head.goto(goto)
         self.distance = 10
         self.turn = 90
@@ -69,12 +69,12 @@ class Snake:
         self.pen.write('Player {}  Score: {}'.format(self.player, self.score), align='center', font=('courier', 24, 'normal'))
 
         
-     
-    def move(self,forward_key, back_key, left_key, right_key):
+    
+    def move(self, back_key, left_key, right_key):
         self.sc = Screen()
         self.sc.listen()
 
-        self.sc.onkey(self.forward,forward_key)
+        #self.sc.onkey(self.forward,forward_key)
         self.sc.onkey(self.backward, back_key)
         ####self.sc.onkey(self.backward, 'Down')
         self.sc.onkey(self.left, left_key)
@@ -84,7 +84,38 @@ class Snake:
         #self.showturtle()
 
     def forward(self):
-        self.head.forward(self.distance)
+        while True:
+            self.head.forward(self.distance)
+            if self.head.xcor()>290 or self.head.xcor()<-290 or self.head.ycor()>290 or self.head.ycor()<-290:
+            
+            #time.sleep(1)
+            #self.head.color('black')
+                self.head.goto(0,0)
+            
+                self.head.direction = 'stop'
+
+                self.score = 0
+                self.pen.clear()
+                self.pen.write('Player {}  Score: {}'.format(self.player, self.score), align='center', font=('courier', 24, 'normal'))
+ 
+            self.random = random
+            if self.head.distance(self.food) < 20:
+                
+                self.score += 10 
+                self.pen.clear()
+                
+                y = self.random.randint(-290, 290)
+                x = self.random.randint(-290, 290)
+                self.food.goto(x,y)
+
+                self.pen.write('Player {}  Score: {}'.format(self.player, self.score), align='center', font=('courier', 24, 'normal'))
+                
+                if self.score > 25:
+                    #self.pen.clear()
+                    self.pen.goto(0,0)
+                    self.pen.color('black')
+                    self.pen.write('Player {}  is the winner!'.format(self.player), align='center', font=('courier', 30, 'bold'))
+                    wn.mainloop()
 
     def backward(self):
         self.head.backward(self.distance)
@@ -95,19 +126,8 @@ class Snake:
     def right(self):
         self.head.right(self.turn)
 
-    def border_c(self):
-        if self.head.xcor()>290 or self.head.xcor()<-290 or self.head.ycor()>290 or self.head.ycor()<-290:
-            
-            #time.sleep(1)
-            #self.head.color('black')
-            self.head.goto(0,0)
+    #def border_c(self):
         
-            self.head.direction = 'stop'
-
-            self.score = 0
-            self.pen.clear()
-            self.pen.write('Player {}  Score: {}'.format(self.player, self.score), align='center', font=('courier', 24, 'normal'))
- 
     #def test_m(self):
         #x = self.head.xcor()
         #self.head.setx(self.head.xcor() + 100)
@@ -119,7 +139,7 @@ class Snake:
         #self.food.goto(0,0)
         
     
-    def scores(self):
+    #def scores(self):
         #self.color = ['red', 'yellow', 'blue', 'black']
         #self.player = player
         
@@ -130,25 +150,7 @@ class Snake:
 
         #pen.clear()
         #pen.write('{} Score:  0'.format(self.player), align='center', font=('courier', 24, 'normal'))
-        self.random = random
-        #self.food = food
-        if self.head.distance(self.food) < 20:
-            
-            self.score += 10 
-            self.pen.clear()
-            
-            y = self.random.randint(-290, 290)
-            x = self.random.randint(-290, 290)
-            self.food.goto(x,y)
-
-            self.pen.write('Player {}  Score: {}'.format(self.player, self.score), align='center', font=('courier', 24, 'normal'))
-            
-            if self.score > 5:
-                #self.pen.clear()
-                self.pen.goto(0,0)
-                self.pen.color('black')
-                self.pen.write('Player {}  is the winner!'.format(self.player), align='center', font=('courier', 30, 'bold'))
-                wn.mainloop()
+       
             #self.pen.clear()
     #def main(self):
         #self.sc.mainloop()
@@ -156,20 +158,24 @@ yellow = Snake((-20,20), 'yellow', '1', -150, 260, (100, 0))
 
 blue = Snake((20,20), 'blue', '2', 150, 260, (-100, 0))
 
+
+
 while True:
-
-    wn.update()
-
-    #red = Snake((20, -20), 'red', 'w', 's', 'a', 'd')
-    yellow.move('w', 's', 'a', 'd')
+    yellow.move('s', 'a', 'd')
+        
     
-    yellow.border_c()
-    blue.move('i', 'k', 'j', 'l')
+    blue.move('k', 'j', 'l')
+    wn.update()
+    yellow.forward()
+    blue.forward()
+#yellow.border_c()
+    #red = Snake((20, -20), 'red', 'w', 's', 'a', 'd')
+    
     #blue.change()
     #blue.test_m()
-    blue.border_c()
-    yellow.scores()
-    blue.scores()
+    #blue.border_c()
+    #yellow.scores()
+    #blue.scores()
     #yellow.food_c()
     #blue.food_c()
     #blue.border_c()
